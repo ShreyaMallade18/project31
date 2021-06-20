b9a83999622e3e29fd9999e28daf177c53dbb30c
ShreyaMallade18 <79143641+ShreyaMallade18@users.noreply.github.com>
2021-06-20 20:06:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+var divisions = [];
+var divisionsHeight = 300;
+var particles =[];
+var plinkos =[];
+
+ function preload(){
+
+
+ }
+
+function setup() {
+  createCanvas(800,700);
+  engine = Engine.create();
+  world = engine.world;
+
+
+  ground = new Ground(400,690,800,20);
+
+  /*ball 1 = new particle(200,200,10);
+  ball 2 = new particle(200,210,10);
+  ball 3 = new particle(200,220,10);
+  ball 4 = new particle(200,230,10);
+  ball 5 = new particle(200,240,10);
+  ball 4 = new particle(200,250,10);*/
+
+  for (var i =0; i<=width; i= i+80){
+    divisions.push(new Division(i,height-divisionsHeight/2,10,divisionsHeight));
+  }
+ 
+  for(var j=75; j<=width; j =j+50){
+    plinkos.push(new Plinko(j,75))
+  }
+
+  for(var j=50; j<=width-10; j =j+50){
+    plinkos.push(new Plinko(j,175))
+  }
+
+  for(var j=75; j<=width; j =j+50){
+    plinkos.push(new Plinko(j,275))
+  }
+
+  for(var j=50; j<=width-10; j =j+50){
+    plinkos.push(new Plinko(j,375))
+  }
+
+//for()
+
+Engine.run(engine)
+
+
+
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  drawSprites();
+
+
+  for(var n = 0; n<divisions.length; n++){
+    divisions[n].display();
+  }
+
+ if(frameCount%60 === 0){
+   particles.push(new Particle(random(width/2-30), width/2+30),10,10);
+ }
+
+ for(var h = 0; h<particles.length; h++){
+  particles[h].display();
+}
+
+for(var j = 0; j<plinkos.length; j++){
+  plinkos[j].display();
+}
+
+ground.display();
+
+
+
+  
+}
